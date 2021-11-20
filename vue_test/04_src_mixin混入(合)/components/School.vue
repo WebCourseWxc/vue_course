10c62c9d9a7143f4e03ab9d32a67092548acd87e
wdcdz <812150635@qq.com>
2021-11-20 17:01:30 +0800

    vue中mixin混合

---
+++ vue_test/04_src_mixin混入(合)/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h2 @click="showName">学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+	</div>
+</template>
+
+<script>
+	//引入一个hunhe
+	// import {hunhe,hunhe2} from '../mixin'
+
+	export default {
+		name:'School',
+		data() {
+			return {
+				name:'岳阳市第13中学',
+				address:'岳阳市奇家岭'
+			}
+		},
+		// mixins:[hunhe,hunhe2],
+	}
+</script>
