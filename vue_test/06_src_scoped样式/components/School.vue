2bcd8cf7803e0c12ba27a8217094e5cbb344bbac
wdcdz <812150635@qq.com>
2021-11-20 17:19:41 +0800

    scope样式的更改

---
+++ vue_test/06_src_scoped样式/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="demo">
+		<h2 class="title">学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'School',
+		data() {
+			return {
+				name:'岳阳市第13中学',
+				address:'岳阳市奇家岭'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.demo{
+		background-color: skyblue;
+	}
+</style>
